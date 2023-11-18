843f1741f4dd35dcd16f68090e07a5b5570bb3cf
peacepiece7 <peacepiece7@naver.com>
2023-11-19 04:24:52 +0900

    styles : 로딩 컴포넌트 일부 수정

--- redux-styled/src/layouts/RootLayout.tsx
+++ redux-styled/src/layouts/RootLayout.tsx
@@ -1,5 +1,7 @@
 import { Loading } from '@/components/Common/Loading'
 import { useTodos } from '@/hooks/fetchTodo'
+import { useAppSelector } from '@/hooks/redux'
+import { selectUser } from '@/redux/features/user/userReducer'
 
 import { font, size } from '@/styles/variable'
 import styled from 'styled-components'
@@ -15,9 +17,13 @@ const Wrapper = styled.div`
 `
 
 export const RootLayout = ({ children }: RootLayoutProps) => {
-  const { todos, isLoading, isError } = useTodos()
-  if (isLoading) return <Loading text={'투두 리스트 불러오는 중'} />
-  if (isError) throw new Error('투두 리스트를 불러오는데 실패했습니다.')
-  console.log('strict 모드 이기 떄문에 todos는 두 번 호출됩니다. ', todos)
+  const { isLoading, isError } = useTodos()
+  const { user } = useAppSelector(selectUser)
+  if (isLoading) {
+    return <Loading text={`${user.nickname}님의 투두 리스트 불러오는 중...`} />
+  }
+  if (isError) {
+    throw new Error('투두 리스트를 불러오는데 실패했습니다.')
+  }
   return <Wrapper>{children}</Wrapper>
 }
--- redux-styled/src/templates/ProtectedRoute.tsx
+++ redux-styled/src/templates/ProtectedRoute.tsx
@@ -4,13 +4,14 @@ import { selectUser } from '@/redux/features/user/userReducer'
 import { fetchUserThunk } from '@/redux/features/user/userThunk'
 import { Navigate, Outlet } from 'react-router-dom'
 
+// * 상황에 따라서 특정 컴포넌트만 Wrapping 해도 됩니다.
 export const ProtectedRoute = () => {
   const dispatch = useAppDispatch()
   const { status } = useAppSelector(selectUser)
 
   if (status === 'pending') {
     dispatch(fetchUserThunk())
-    return <Loading />
+    return <Loading text='로그인 여부 체크 중...' />
   }
 
   if (status === 'rejected') {
